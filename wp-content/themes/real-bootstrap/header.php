85e3978c76005c41c81d3a9561bf41d21178f8f0
Patrick Fischer <fischerpl@mail.lipscomb.edu>
2016-09-29 15:49:16 +0000

    Addded Google Analytics plugin.

--- wp-content/themes/real-bootstrap/header.php
+++ wp-content/themes/real-bootstrap/header.php
@@ -21,7 +21,7 @@
 	<?php wp_head(); ?>
 </head>
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<div class="site-inner">
